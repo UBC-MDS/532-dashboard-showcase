ba90d8d87d74be07e04c4eb0eb81510164fbf82b
Joel Ostblom <joel.ostblom@gmail.com>
2024-04-10 02:53:36 -0700

    Update with local data path

--- src/data.py
+++ src/data.py
@@ -1,4 +1,7 @@
-from vega_datasets import data
+# from vega_datasets import data
+import pandas as pd
 
-print('data file')
-cars = data.cars()
+# print('data file')
+# cars = data.cars()
+# cars.to_csv('cars.csv', index=False)
+cars = pd.read_csv('../data/cars.csv')
